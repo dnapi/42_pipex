f1c710c8d15166a776974e8edc74058accacb669
Alex <pimikov@gmail.com>
2024-01-07 17:58:26 +0200

    test for functions

--- readme.txt
+++ readme.txt
@@ -1,3 +1,7 @@
 
 Advanced Programming in the Unix Environment, Richard Stevens
 codes http://www.apuebook.com/code3e.html
+
+
+https://reactive.so/post/42-a-comprehensive-guide-to-pipex
+https://dev.to/herbievine/42-a-comprehensive-guide-to-pipex-5165
--- test_perror.c
+++ test_perror.c
@@ -6,7 +6,8 @@
 
 int	main(int argc, char *argv[])
 {
-	printf("EACCES: %s\nENOENT=%d\n", strerror(EACCES),ENOENT);
+	printf("EACCES: %s\nEACCES=%d\n", strerror(EACCES),EACCES);
+	printf("ENOENT: %s\nENOENT=%d\n", strerror(ENOENT),ENOENT);
 	errno = ENOENT;
 	perror(argv[0]);
 	exit(0);
