ef35c551866ebd5577807423ec62740f037e3346
Alex <pimikov@gmail.com>
2024-01-11 06:44:05 +0200

    commants

--- src/pipex.c
+++ src/pipex.c
@@ -1,66 +1,84 @@
-#include "pipex.h"
-#include "errno.h"
+//#include "pipex.h"
+#include "../include/pipex.h"
+#include <stdio.h>
+#include <unistd.h>
 
-int	main(int argc, char *argv[], char *envp[])
+void	pipex(int argc, char *argv[], char *envp[])
 {
-    int num = 3;
-	int	i;
-	int		fd_file;
-	int		fd[2];
-	pid_t	pid[5];
-    char *cat = "/bin/cat";
-    char *args[] = { "cat", NULL };
-//   char *sleep = "/bin/cat";
-//   char *args_sleep[] = { "sleep", "2",  NULL };
+  int i;
+  int fd[2];
+  pid_t pid;
 
-	fd_file = open(argv[1], O_RDONLY);
-	if (fd_file == -1)
+  char  **arg;
+  char  *cmd;
+  int flag;
+
+	fd[0] = open(argv[1], O_RDONLY);
+	if (fd[0] == -1)
+  {
+    ft_printf("er num=%d\n", ENOENT);
 		strerror(ENOENT);
-	dup2(fd_file, STDIN_FILENO);
-	close(fd_file);
-	(void) envp;
+    perror(argv[1]);
+		exit(ENOENT);
+  }
+	dup2(fd[0], STDIN_FILENO);
+	close(fd[0]);
 
 	i = -1;
-    while (++i < num)
+    while (++i < argc - 3)
 	{
 		pipe(fd);
-		if (i == num - 1)
+		if (i == argc - 4)
 		{
 			close(fd[1]);
 			fd[1]  = open(argv[argc - 1], O_WRONLY | O_TRUNC | O_CREAT, 0644);
 		}
-        pid[i] = fork();
-		if (pid[i] == 0) // child
+        pid = fork();
+		if (pid == 0) // child
 		{
-			write(2, "start\n", 6);
+     // exit(127);
 			dup2(fd[1], STDOUT_FILENO);
 			close(fd[0]);
 			close(fd[1]);
-//			sleep(25);
-			execve(cat, args, NULL);
-			write(2, "end\n", 4);
-            exit(0);
+      arg = ft_split(argv[i + 2], ' ');
+      cmd = ft_strjoin("/bin/", *arg);
+			execve(cmd, arg, envp);
+      perror("execve"); 
+      exit(42);
 		}
-		else if (pid[i] > 0)
+		else if (pid > 0)
 		{
 			dup2(fd[0], STDIN_FILENO);
 			close(fd[0]);
 			close(fd[1]);
 		}
-        else // fork error 
-		{
-            perror("fork");
-            return 1;
-        }
-    }
+    else
+     perror("fork");
+  }
+  
+  flag = 11;
+	waitpid(pid, &flag, 0);
+//	waitpid(pid[2], NULL, 0);
+/*  if (flag == 127)
+    write(2, "error 127\n", 10);
+  else
+    write(2, "error != 127\n", 14);
+*/
+  exit (flag);
 
+}
 
-	waitpid(pid[0], NULL, 0);
-	waitpid(pid[1], NULL, 0);
-	waitpid(pid[2], NULL, 0);
+int	main(int argc, char *argv[], char *envp[])
+{
+ //   char *cat = "/bin/cat";
+  //  char *args[] = { "cat", NULL };
+  //   char *sleep = "/bin/cat";
+  //
 
-	write(2, "end\n", 4);
+  pipex(argc, argv, envp);
+
+//	write(2, "end\n", 4);
 //	while (1)
 //		;
-	exit (0);
+	exit (24);
 }
